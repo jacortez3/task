ac64d63d311edfeb4ad5bf3f18491ec993af3d33
jacortez3 <jacortez3@espe.edu.ec>
2024-02-02 03:15:58 -0500

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,63 @@
 // App.jsx
 import { createClient } from '@supabase/supabase-js';
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useState, useEffect } from "react";
 import TaskList from './components/TaskList';
 import TaskForm from './components/TaskForm';
 import './index.css';
 
-// Configurar el cliente Supabase
 const supabaseUrl = import.meta.env.VITE_REACT_APP_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_REACT_APP_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-const App = () => {
-  return (
-    <div>
-      
-      <h1>Task Manager</h1>
-      <TaskForm supabase={supabase} />
-      <TaskList supabase={supabase} />
-    </div>
-  );
-};
 
-export default App;
+export default function App() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!session) {
+    return (
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <Auth
+            supabaseClient={supabase}
+            appearance={{ theme: ThemeSupa }}
+            providers={["facebook"]}
+          />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <div>Logged in!</div>
+        <button onClick={() => supabase.auth.signOut()}>Sign out</button>
+
+        <h1>Task Manager</h1>
+        <TaskForm supabase={supabase} />
+        <TaskList supabase={supabase} />
+      </div>
+    );
+  }
+}
+
